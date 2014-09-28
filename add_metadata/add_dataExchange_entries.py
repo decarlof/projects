2171e829f257add0ac6d2bb8f1a373d4b468056a
Francesco De Carlo <decarlof@gmail.com>
2014-09-28 18:55:28 -0500

    first commit

---
+++ add_metadata/add_dataExchange_entries.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+.. module:: add_dataExchange_entries.py
+   :platform: Unix
+   :synopsis: add Data Exchange entries to an HDF5 file
+   :INPUT
+      Data Exchange file name 
+
+.. moduleauthor:: Francesco De Carlo <decarlof@gmail.com>
+
+
+""" 
+
+from data_exchange import DataExchangeFile, DataExchangeEntry
+import os
+
+def main():
+    #****************************************************************************
+    hdf5_file_name = '/local/dataraid/databank/dataExchange/tmp/ALS_test.h5'
+    if (hdf5_file_name != None):
+        if os.path.isfile(hdf5_file_name):
+            print "Data Exchange file: [%s] already exists", hdf5_file_name
+            # Write the Data Exchange HDF5 file.
+            # Open DataExchange file
+            f = DataExchangeFile(hdf5_file_name, mode='a') 
+
+        else:
+            # Create new folder.
+            dirPath = os.path.dirname(hdf5_file_name)
+            if not os.path.exists(dirPath):
+                os.makedirs(dirPath)
+
+            # Get the file_name in lower case.
+            lFn = hdf5_file_name.lower()
+
+            # Split the string with the delimeter '.'
+            end = lFn.split('.')
+
+            # Write the Data Exchange HDF5 file.
+            # Open DataExchange file
+            f = DataExchangeFile(hdf5_file_name, mode='w') 
+
+            print "Creating Data Exchange File [%s]", hdf5_file_name
+
+        # Create HDF5 subgroup
+        # /measurement/instrument
+        f.add_entry( DataExchangeEntry.instrument(name={'value': 'APS 2-BM'}) )
+
+        f.add_entry( DataExchangeEntry.source(name={'value': 'Advanced Photon Source'},
+                                            date_time={'value': "2012-07-31T21:15:23+0600"},
+                                            beamline={'value': "2-BM"},
+                                            current={'value': 101.199, 'units': 'mA', 'dataset_opts': {'dtype': 'd'}},
+                                            energy={'value': 7.0, 'units':'GeV', 'dataset_opts': {'dtype': 'd'}},
+                                            mode={'value':'TOPUP'}
+                                            )
+        )
+        # Create HDF5 subgroup
+        # /measurement/instrument/attenuator
+        f.add_entry( DataExchangeEntry.attenuator(thickness={'value': 1e-3, 'units': 'm', 'dataset_opts': {'dtype': 'd'}},
+                                                type={'value': 'Al'}
+                                                )
+            )
+
+        # Create HDF5 subgroup
+        # Create HDF5 subgroup
+        # /measurement/instrument/monochromator
+        f.add_entry( DataExchangeEntry.monochromator(type={'value': 'Multilayer'},
+                                                    energy={'value': 19.26, 'units': 'keV', 'dataset_opts': {'dtype': 'd'}},
+                                                    energy_error={'value': 1e-3, 'units': 'keV', 'dataset_opts': {'dtype': 'd'}},
+                                                    mono_stripe={'value': 'Ru/C'},
+                                                    )
+            )
+
+
+        # Create HDF5 subgroup
+        # /measurement/experimenter
+        f.add_entry( DataExchangeEntry.experimenter(name={'value':"Jane Waruntorn"},
+                                                    role={'value':"Project PI"},
+                                                    affiliation={'value':"University of California"},
+                                                    facility_user_id={'value':"64924"},
+
+                        )
+            )
+
+        f.add_entry(DataExchangeEntry.objective(manufacturer={'value':'Zeiss'},
+                                                model={'value':'Plan-NEOFLUAR 1004-072'},
+                                                magnification={'value':5, 'dataset_opts': {'dtype': 'd'}},
+                                                numerical_aperture={'value':0.5, 'dataset_opts': {'dtype': 'd'}},
+                                            )
+            )
+
+        f.add_entry(DataExchangeEntry.scintillator(manufacturer={'value':'Crytur'},
+                                                    serial_number={'value':'12'},
+                                                    name={'value':'LuAg '},
+                                                    type={'value':'LuAg'},
+                                                    scintillating_thickness={'value':50e-6, 'dataset_opts': {'dtype': 'd'}},
+                                                    substrate_thickness={'value':50e-6, 'dataset_opts': {'dtype': 'd'}},
+                )
+            )
+
+        # Create HDF5 subgroup
+        # /measurement/experiment
+        f.add_entry( DataExchangeEntry.experiment( proposal={'value':"GUP-34353"},
+                                                    activity={'value':"32-IDBC-2013-106491"},
+                                                    safety={'value':"106491-49734"},
+                    )
+            )
+        sample_name = "test_of_sample_name"
+
+        if (sample_name != None):
+            f.add_entry( DataExchangeEntry.sample(root='/measurement', name={'value':sample_name}, description={'value':'added by AddEntry.py'}), overwrite=True)
+
+    f.close()
+ 
+if __name__ == "__main__":
+    main()
+
---
+++ add_metadata/schedule.py
@@ -0,0 +1,197 @@
+"""
+.. module:: schedule.py
+   :platform: Unix
+   :synopsis: Interact with information from the APS online schedule.
+
+.. moduleauthor:: David Vine <djvine@gmail.com>
+
+This module is largely John Hammonds work to which I'll be adding
+some scripts as needed.
+"""
+
+from suds.wsse import Security, UsernameToken
+from suds.client import Client
+from suds.transport.https import HttpAuthenticated
+import sys
+import datetime
+import traceback
+import urllib2
+import httplib
+from xml.sax import SAXParseException
+#import ipdb
+from collections import defaultdict
+
+debug = False
+""" You must use the APS web password. You can check it by logging into
+the proposal system. Be careful because this system also accepts LDAP
+account info."""
+username = 'BADGE NUMBER'
+password = 'APS WEB PASSWORD'
+username = 'decarlo'
+password = '!1Dolore'
+# Uncomment this for INTERNAL network
+base = 'https://schedule.aps.anl.gov:8443/beamschedds/springws/'
+# Uncomment this for EXTERNAL network
+#base = 'https://schedule.aps.anl.gov/beamschedds/springws/'
+
+class HTTPSConnectionV3(httplib.HTTPSConnection):
+    def __init__(self, *args, **kwargs):
+        httplib.HTTPSConnection.__init__(self, *args, **kwargs)
+
+    def connect(self):
+        sock = socket.create_connection((self.host, self.port), self.timeout)
+        if self._tunnel_host:
+            self.sock = sock
+            self._tunnel()
+        try:
+            print ("using connection")
+            self.sock = ssl.wrap_socket(sock, self.key_file, self.cert_file, \
+                                        ssl_version=ssl.PROTOCOL_SSLv3)
+        except ssl.SSLError, e:
+            print("Trying SSLv3.")
+            self.sock = ssl.wrap_socket(sock, self.key_file, self.cert_file, \
+                                        ssl_version=ssl.PROTOCOL_SSLv23)
+
+class HTTPSHandlerV3(urllib2.HTTPSHandler):
+    def https_open(self, req):
+        print "using this opener"
+        return self.do_open(HTTPSConnectionV3, req)
+
+def setSoapHeader(client, username, password):
+    security = Security()
+    token = UsernameToken(username, password)
+    token.setcreated()
+    security.tokens.append(token)
+    if debug:
+        print security
+    client.set_options(wsse=security)
+
+def findRunName(startDate, endDate):
+    """Find the official run name for the run that spans the
+    given startDate and endDate
+
+    Returns string."""
+    runScheduleServiceClient, beamlineScheduleServiceClient = setup_connection()
+    try:
+        result = runScheduleServiceClient.service.findAllRuns()
+    except Exception:
+        print "ERROR in findRunName"
+        sys.exit(2)
+    except soapFault:
+        print "ERROR in findRunName"
+        print soapFault
+        sys.exit(2)
+    runArray = result.run
+    runName = None
+    for run in runArray:
+
+        try:
+            if startDate >= run.startTime and endDate <= run.endTime:
+                runName = run.runName
+                break
+        except Exception as ex:
+            print "ERROR caught in findRunName:" + str(ex)
+            print startDate
+            print run.startTime
+            print endDate
+            print run.endTime
+            raise ex
+    return runName
+
+
+def findBeamlineSchedule(beamlineName, runName):
+    """Find beamline schedule for given beamlineName and runName"""
+
+    runScheduleServiceClient, beamlineScheduleServiceClient = setup_connection()
+    try:
+        result  = beamlineScheduleServiceClient.service.findBeamlineSchedule(beamlineName, runName)
+    except SAXParseException as ex:
+        print "ERROR in findBeamlineSchedule\n"
+        traceback.print_exc()
+        sys.exit(2)
+
+    return result
+
+def findBeamtimeRequestsByBeamline(beamlineName, runName):
+    """Find beamline schedule for given beamlineName and runName
+
+    Returns schedule object."""
+    try:
+        result  = beamlineScheduleServiceClient.service.findBeamtimeRequestsByBeamline(beamlineName, runName)
+    except SAXParseException:
+        print "ERROR in findBeamtimeRequestsByBeamline"
+    except Exception:
+        print "ERROR in findBeamtimeRequestByBeamline\n"
+        traceback.print_exc()
+        sys.exit(2)
+    return result
+
+def setup_connection():
+    result = urllib2.install_opener(urllib2.build_opener(HTTPSHandlerV3()))
+
+    beamlineScheduleServiceURL = base + \
+         'beamlineScheduleService/beamlineScheduleWebService.wsdl'
+
+    runScheduleServiceURL = base + \
+         'runScheduleService/runScheduleWebService.wsdl'
+
+    try:
+        credentials = dict(username=username, password=password)
+        t = HttpAuthenticated(**credentials)
+        if debug:
+            print t.u2handlers()
+            print t.credentials()
+        runScheduleServiceClient = Client(runScheduleServiceURL)
+        runScheduleServiceClient.options.cache.setduration(seconds=10)
+        result = setSoapHeader(runScheduleServiceClient, username, password)
+        beamlineScheduleServiceClient = Client(beamlineScheduleServiceURL)
+        beamlineScheduleServiceClient.options.cache.setduration(seconds=10)
+        result = setSoapHeader(beamlineScheduleServiceClient, username, password)
+    except Exception, ex:
+        print "CANNOT OPEN SERVICES:" + str(ex)
+        raise
+        exit(-1)
+
+    return runScheduleServiceClient, beamlineScheduleServiceClient
+
+def get_users(beamline='2-ID-E', date=None):
+    runScheduleServiceClient, beamlineScheduleServiceClient = setup_connection()
+    if not date:
+        date = datetime.datetime.now()
+    run_name = findRunName(date, date)
+    schedule = findBeamlineSchedule(beamline, run_name)
+
+    print run_name
+    print schedule
+
+    events = schedule.activities.activity
+    users = defaultdict(dict)
+    
+    print events
+    print users
+
+    for event in events:
+        try:
+            if event.activityType.activityTypeName in ['GUP', 'PUP', 'rapid-access']:
+                if date >= event.startTime and date <= event.endTime:
+                        for experimenter in event.beamtimeRequest.proposal.experimenters.experimenter:
+                            for key in experimenter.__keylist__:
+                                users[experimenter.lastName][key] = getattr(experimenter, key)
+        except:
+            print "ipdb except"
+            #ipdb.set_trace()
+            raise
+
+    return users
+
+
+if __name__ == '__main__':
+
+    now = datetime.datetime.now()
+    runScheduleServiceClient, beamlineScheduleServiceClient = setup_connection()
+    print "Current Run Name:", findRunName(now, now)
+    
+    experiment_date = datetime.datetime(2014, 10, 16, 8)
+    print "Experiment date:", experiment_date
+    print "User:", get_users('2-ID-B', experiment_date)
+
