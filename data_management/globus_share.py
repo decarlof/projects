067e5360f53adf1f3cd2b6b053da870575ff0950
Francesco De Carlo <decarlof@gmail.com>
2015-10-02 00:46:07 -0500

    first commit

--- data_management/globus_share.py
+++ data_management/globus_share.py
@@ -3,32 +3,54 @@
 .. module:: globus_share.py
    :platform: Unix
    :synopsis:   
-                Share via Globus a data directory with the users listed in the proposal 
+        Share via Globus a data directory with the users email 
    :INPUT
-      Date of the experiments 
+        folder, email 
 
 .. moduleauthor:: Francesco De Carlo <decarlof@gmail.com>
 
 """ 
 
 import os
+import sys, getopt
 
-globus_user = 'decarlo'
+# see README.txt to set a globus personal shared folder
+globus_user = 'usr32idc'
 globus_share_folder = "/local/dataraid/"
+globus_share = "#dataraid"
 globus_ssh = 'ssh ' + globus_user + '@cli.globusonline.org'
 
-      
-if __name__ == "__main__":
-
-    folder = "dm"
-    email = "decarlo@aps.anl.gov"
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hf:e:",["ffolder=","eemail="])
+    except getopt.GetoptError:
+        print 'test.py -f <folder> -e <email>'
+        #print 'test.py -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'globus_share.py -f <folder> -e <email>'
+            sys.exit()
+        elif opt in ("-f", "--ffolder"):
+            inputfolder = arg
+        elif opt in ("-e", "--eemail"):
+            inputemail = arg
+    print 'Shared Folder is: ', inputfolder
+    print 'User e-mail is: ', inputemail
     
-    globus_add = "acl-add " + globus_user + "#databank" + os.sep + folder + os.sep + " --perm r --email " + email
+    #folder = "dm" + os.sep
+    #email = "decarlo@aps.anl.gov"
+    
+    globus_add = "acl-add " + globus_user + globus_share + os.sep + inputfolder  + " --perm r --email " + inputemail
 
     cmd = globus_ssh + " " + globus_add
     print cmd
     #os.system(cmd)
-    print "\n\n======================================"
-    print "Check your email to download the data "
-    print "======================================"
+    print "Download link sent to: ", inputemail
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
 
